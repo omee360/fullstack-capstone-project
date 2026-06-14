f647ae3c13a912b2039190dcecef0cd410941809
omee360 <omee360@gmail.com>
2026-06-14 11:41:36 -0400

    register endpoint basic structure completed

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -12,10 +12,42 @@ const logger = pino();  // Create a Pino logger instance
 
 dotenv.config();
 
-//Step 1 - Task 4: Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
-//Step 2
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB collection
+        const collection = await db.collection("users");
+
+        //Task 3: Check for existing email
+        const existingEmail = await collection.findOne({ email: req.body.email })
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email = req.body.email;
+
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date()
+        });
+
+        const payload = {
+            user: {
+                id: newUser.insertedId
+            }
+        };
+        logger.info('User registered successfully');
+        res.json({ authtoken, email });
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
 });
+
 
 module.exports = router;
